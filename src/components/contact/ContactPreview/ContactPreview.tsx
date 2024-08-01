52e53eb3ef75ee08f1ed8f10f8597df3e5c8ec6f
Shankrie <gyngga@gmail.com>
2024-08-01 13:21:50 +0300

    Contact preview loading and error handling

--- src/components/contact/ContactPreview/ContactPreview.tsx
+++ src/components/contact/ContactPreview/ContactPreview.tsx
@@ -10,19 +10,13 @@ type Props = {
 };
 
 export const ContactPreview = ({ id }: Props) => {
-  const { data } = useQuery({
+  const { data, isPending, isError, isSuccess } = useQuery({
     queryKey: ["contact", id],
     placeholderData: keepPreviousData,
     queryFn: () => getContact(id),
   });
 
   const { t } = useTranslation();
-
-  const contact = data?.data;
-
-  if (!contact) {
-    return null;
-  }
 
   return (
     <div className="w-full max-w-[330px] bg-white shadow-normal rounded-sm overflow-hidden">
@@ -32,38 +26,48 @@ export const ContactPreview = ({ id }: Props) => {
         alt={t("contact.photo")}
       />
       <div className="flex flex-col gap-[16px] p-[16px] text-center">
-        <div className="text-lg leading-[32px] text-text">
-          {formatName(contact.name, contact.surname)}
-        </div>
-        <table className="w-full text-textLight">
-          <tbody>
-            {[
-              {
-                label: t("contact.name"),
-                value: formatName(contact.name, contact.surname),
-              },
-              {
-                label: t("contact.city"),
-                value: contact.city,
-              },
-              {
-                label: t("contact.email"),
-                value: (
-                  <Link href={`mailto:${contact.email}`}>{contact.email}</Link>
-                ),
-              },
-              {
-                label: t("contact.phone"),
-                value: contact.phone,
-              },
-            ].map((row) => (
-              <tr className="text-xs leading-[16px]" key={row.label}>
-                <td className="px-[16px] text-right">{row.label}:</td>
-                <td className="px-[16px] text-left">{row.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {isPending && <div className="text-center">{t("general.loading")}</div>}
+
+        {isError && <div className="text-center">{t("general.loadError")}</div>}
+
+        {isSuccess && (
+          <>
+            <div className="text-lg leading-[32px] text-text">
+              {formatName(data.data.name, data.data.surname)}
+            </div>
+            <table className="w-full text-textLight">
+              <tbody>
+                {[
+                  {
+                    label: t("contact.name"),
+                    value: formatName(data.data.name, data.data.surname),
+                  },
+                  {
+                    label: t("contact.city"),
+                    value: data.data.city,
+                  },
+                  {
+                    label: t("contact.email"),
+                    value: (
+                      <Link href={`mailto:${data.data.email}`}>
+                        {data.data.email}
+                      </Link>
+                    ),
+                  },
+                  {
+                    label: t("contact.phone"),
+                    value: data.data.phone,
+                  },
+                ].map((row) => (
+                  <tr className="text-xs leading-[16px]" key={row.label}>
+                    <td className="px-[16px] text-right">{row.label}:</td>
+                    <td className="px-[16px] text-left">{row.value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
       </div>
     </div>
   );
--- src/translations/en/general.ts
+++ src/translations/en/general.ts
@@ -2,4 +2,6 @@ export default {
   filter: "Filter",
   pageNotFound: "Page not found",
   selectVisibleColumns: "Select visible columns",
+  loading: "Loading...",
+  loadError: "Could not load data",
 };
